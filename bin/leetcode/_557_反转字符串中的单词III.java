5bad6cd38a04c5dd73dfeadba8b114d32094e469
FlyGreyWolf <504574519@qq.com>
2020-08-30 14:39:35 +0800

    刷题中...

---
+++ bin/leetcode/_557_反转字符串中的单词III.java
@@ -0,0 +1,52 @@
+package bin.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入："Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
+ *  
+ *
+ * 提示：
+ *
+ * 在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-words-in-a-string-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author FlyGreyWolf
+ * @since 2020/8/30
+ */
+public class _557_反转字符串中的单词III {
+
+    public String reverseWords(String s) {
+
+        LinkedList<Character> queue = new LinkedList<>();
+        String res = "";
+        char[] sarr = s.toCharArray();
+
+        for(int i=0; i<sarr.length; i++) {
+            if(sarr[i] != ' ') {
+                queue.addLast(sarr[i]);
+            } else {
+                while(!queue.isEmpty()) {
+                    res = res + queue.pollLast();
+                }
+                res = res + " ";
+            }
+
+        }
+        while(!queue.isEmpty()) {
+            res = res + queue.pollLast();
+        }
+
+        return res;
+    }
+}
